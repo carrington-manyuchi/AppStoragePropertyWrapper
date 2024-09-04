c5e3ea111f30e08825fba07c7d21b1e256122724
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-09-04 12:03:18 +0200

    Initial Commit

---
+++ AppStoragePropertyWrapper/AppStoragePropertyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoragePropertyWrapperApp.swift
+//  AppStoragePropertyWrapper
+//
+//  Created by Manyuchi, Carrington C on 2024/09/04.
+//
+
+import SwiftUI
+
+@main
+struct AppStoragePropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
